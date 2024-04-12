95cc95fe3f8a4348caa494b819097df9cadfe1c8
Igor Juncal Yazigi <73030677+IgorJY@users.noreply.github.com>
2024-04-12 15:01:37 -0300

    Add files via upload

---
+++ import.js
@@ -0,0 +1,36 @@
+const prompt = require("prompt-sync")();
+const { table } = require("console");
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const readline = require("readline");
+
+// caminho do csv
+const path = "TesteNodeJs 1.csv";
+
+// criando a stream de leitura
+const readStream = fs.createReadStream(path);
+
+// criando a incerface readline
+const readInterface = readline.createInterface({
+  input: readStream
+});
+
+// inicializando o array
+const vendas = [];
+
+//lendo as linhas
+readInterface.on("line", (line) => {
+  const row = line.split(",");
+  vendas.push(row);
+});
+
+// fechando a leitura do cvs
+readInterface.on("close", () => {
+  console.log(vendas);
+});
+
+// lidando com erros
+readInterface.on("error", (err) => {
+  console.error("Error reading the CSV file:", err);
+});
+
